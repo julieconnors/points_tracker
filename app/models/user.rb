f48cfa6c2689e0355a88cb08ffe304c6b5a0fab9
julieconnors <juliana.g.connors@gmail.com>
2020-09-28 18:41:18 -0400

    list horse shows method

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,13 @@ class User < ActiveRecord::Base
     has_secure_password
     validates_presence_of :username, :password
 
+    def list_horse_shows
+        horse_shows = []
+        self.horses.each do |horse|
+            horse.horseshows.each do |horseshow|
+                horse_shows << horseshow.name
+            end
+        end
+        horse_shows.uniq
+    end
 end
