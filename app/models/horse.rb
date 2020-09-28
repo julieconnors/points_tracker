a6d1a3a00264855e4ce4b0a8faafeb77efbe35c5
julieconnors <juliana.g.connors@gmail.com>
2020-09-28 15:52:28 -0400

    beginning of user homepage

--- app/models/horse.rb
+++ app/models/horse.rb
@@ -7,4 +7,11 @@ class Horse < ActiveRecord::Base
         point_list = self.prizes.map{|prize| prize.point_total}
         point_list.sum
     end
+
+    def horse_show_list
+        show_ids = self.prizes.map{|prize| prize.horse_show_id}
+        show_ids.map do |id|
+            HorseShow.find(id).name
+        end
+    end
 end
