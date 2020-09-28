958fb331c9ceda44875d40c660dc64659ba91339
julieconnors <juliana.g.connors@gmail.com>
2020-09-28 06:51:01 -0400

    working on login functionality

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,40 @@
 class UsersController < ApplicationController
 
-    get '/users' do 
-        "users"
+    get '/login' do
+        erb :"/users/login"
+    end
+
+    get '/account' do #should I have this or erb in post 'login' ???
+        erb :'/users/home'
+    end
+
+    post '/login' do
+        @user = User.all.find_by(username: params[:username])
+
+        if @user
+            session[:user_id] = @user.id
+
+            erb :"/users/home"
+            #redirect to "/account"
+        else
+            erb :error
+        end
+    end
+    
+    get '/signup' do
+        erb :"/users/signup"
+    end
+
+    post '/signup' do
+        @user = User.create(params)
+        session[:user_id] = @user.id
+
+        redirect to "/users/home"
+    end
+
+    get '/logout' do
+        session.clear
+
+        redirect to '/'
     end
 end
