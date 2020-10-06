ec79d3a5033b0456158f023513ada20e8c3fbe12
julieconnors <juliana.g.connors@gmail.com>
2020-10-06 18:53:15 -0400

    add horseshow date input type to prize forms and adding format date method

--- app/models/horseshow.rb
+++ app/models/horseshow.rb
@@ -9,4 +9,8 @@ class Horseshow < ActiveRecord::Base
     def self.find_by_slug(slug)
         self.all.find { |horseshow| horseshow.slug == slug }
     end
+
+    def format_date
+        Date.parse(self.date).strftime('%-m/%d/%y')
+    end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -3,8 +3,8 @@ User.create(name: "Julie", username: "jules", password: "jules123")
 Horse.create(name: "Lucador", user_id: 1)
 Horse.create(name: "Cosmic", user_id: 1)
 
-Horseshow.create(name: "WIHS", location: "DC", date: "10/14/19",)
-Horseshow.create(name: "The National", location: "KY", date: "11/1/19",)
+Horseshow.create(name: "WIHS", location: "DC", date: "2019-10-25",)
+Horseshow.create(name: "The National", location: "KY", date: "2019-11-1",)
 
 Prize.create(point_total: 1000, horse_id: 1, horseshow_id: 1, user_id: 1)
 Prize.create(point_total: 500,  horse_id: 1, horseshow_id: 2, user_id: 1)
