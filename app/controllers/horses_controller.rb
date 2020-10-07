3d050b6c7d4f859ef5beff80d89d9445e04ca55e
julieconnors <juliana.g.connors@gmail.com>
2020-10-07 07:31:28 -0400

    refactor horses controller and add comments

--- app/controllers/horses_controller.rb
+++ app/controllers/horses_controller.rb
@@ -2,7 +2,6 @@ class HorsesController < ApplicationController
 
     get '/horses' do
         logged_out_redirection
-        @user = current_user
             
         erb :"/horses/index"
     end
@@ -15,14 +14,15 @@ class HorsesController < ApplicationController
 
     post '/horses' do
         logged_out_redirection #do I need login validation on post routes??
-        if !Horse.find_by(name: params[:name].capitalize)
-            @horse = Horse.new(name: params[:name])
-            @horse.user_id = session[:user_id]
-            @horse.save!
+        
+        if !Horse.find_by(name: params[:name].capitalize) #checks if there is already a horse by the same new
+            horse = Horse.new(name: params[:name]) #instantiates new horse
+            horse.user_id = current_user.id #assigns horse user_id to that of current_user
+            horse.save #saves horse with user_id set
 
-            redirect "/horses/#{@horse.slug}"
+            redirect "/horses/#{horse.slug}"
         else
-            erb :"/horses/duplicate-error" # this should be a redirection
+            redirect "/horses/duplicate-error"
         end
     end
 
@@ -42,9 +42,9 @@ class HorsesController < ApplicationController
         logged_out_redirection
             
         @horse = Horse.find_by_slug(params[:slug])
-        @user = User.find(session[:user_id])
-        if @horse.user_id == @user.id
-            erb :"/horses/edit"
+
+        if @horse.user_id == current_user.id #if horse belongs to current user
+            erb :"/horses/edit" #render the edit page
         else
             erb :"/access-error"
         end
@@ -52,11 +52,12 @@ class HorsesController < ApplicationController
 
     patch '/horses/:slug' do
         logged_out_redirection # is this necessary
-        @horse = Horse.find_by_slug(params[:slug])
-        if @horse.user_id == current_user.id
-            @horse.update(name: params[:name])
+        horse = Horse.find_by_slug(params[:slug])
 
-            redirect "/horses/#{@horse.slug}"
+        if horse.user_id == current_user.id #if horse belongs to current_user
+            horse.update(name: params[:name]) #update horse with params data
+
+            redirect "/horses/#{horse.slug}"
         else
             redirect "/access-error" # is this necessary
         end
@@ -64,7 +65,9 @@ class HorsesController < ApplicationController
 
     delete '/horses/:id' do
         logged_out_redirection #is this necessary??
-        horse = Horse.find(params[:id])
+        
+        horse = Horse.find_by(id: params[:id]) #delete form sends id value through params
+        
         if horse.user_id == current_user.id
             horse.destroy
 
