8ce6d1b9c83313706486e013015752778e585ef9
julieconnors <juliana.g.connors@gmail.com>
2021-05-24 07:07:53 -0400

    update file name so it's not overridden

---
+++ app/models/01_slug.rb
@@ -0,0 +1,13 @@
+module Slug
+    module InstanceMethods
+        def slug
+            self.name.downcase.gsub(" ", "-")
+        end
+    end
+
+    module ClassMethods
+        def self.find_by_slug(slug)
+            self.all.find { |n| n.slug == slug }
+        end
+    end
+end
